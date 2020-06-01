2230e83faa38c6de58bbb3f9d8353a1f65b7314d
DawidPartyka <dawpar5@wp.pl>
2020-06-01 05:40:28 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var express = require('express');
 var soap = require('soap');
 var https = require('https');
 var fs = require('fs');
+var mysql = require('mysql');
 /*Dependencies*/
 
 var app = express();
@@ -20,7 +21,6 @@ var options = {
 //console.log("CERT: ", options.cert);
 /*SSL*/
 
-/* Create HTTPS server with SSL from "options" object */
 var serverHttps = https.createServer(options, app).listen(httpsPort, () => {
     console.log(">> Server listening at port " + httpsPort);
 });
@@ -28,70 +28,236 @@ var serverHttps = https.createServer(options, app).listen(httpsPort, () => {
 /*Request | routing handler*/
 app.use('/index', express.static(__dirname + '/clientFiles'));  //main page https://localhost:8000/index/vat.html
 
-// Parse URL-encoded bodies (as sent by HTML forms)
+//Parse URL-encoded bodies (as sent by HTML forms)
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/vats', function(req, res){               //POST request from client
-  req.on('data', chunk => {
-      processNips(chunk.toString().split(' '));     //Sent NIPs in form of array from the POST request
-  });
+app.post('/vats', function(req, res){                           //Handle POST request from page
+  Date.prototype.formatDate = function() {                      //Format date into YYYY-MM-DD
+  let mm = this.getMonth() + 1; //getMonth() is zero-based
+  let dd = this.getDate();
 
-  function processNips(data){
-    //Sanity check
-    if(data){
-      let ctrl = data.length;                       //Just a control value. Number of NIPs sent through POST
+  return [this.getFullYear(),
+          (mm>9 ? '' : '0') + mm,
+          (dd>9 ? '' : '0') + dd
+        ].join('-');
+  };
 
-      let dataObj = {
-        processed: [],                              //Results go here in form of objects {kod, komunikat, nip}
-        add: function(comm, nipFromRes){            //Add received data to array "processed" in form of object
-          this.processed.push({kod: comm.Kod, komunikat: comm.Komunikat, nip: nipFromRes});
-        },
-        finish: function(){
-          dataObj.show(); //console.log saved data
-          //...send back the data
-          res.end(JSON.stringify(this.processed));  //Return data to client in form of JSON
-          res.status(200).send();                   //Everything's OK
-          console.log('stopped');
-        },
-        show: function(){ //Method only to output received data in console
-          let tmp = this.processed;
-          for(let i = 0; i < this.processed.length; i++){
-            console.log('Element ' + i + '{');
-            console.log('\tkod: ' + tmp[i].kod + ',\n\tkomunikat: ' + tmp[i].komunikat + ',\n\tnip: ' + tmp[i].nip + '\n}');
-          }
-        },
-        startCalls: function(data){
-          let tmp = data;
-          let timedCalls = setInterval(function(){ timer() }, 100);  /* Making calls in intervals as given in the documentation.
-                                                                     10 NIPs per second so the functions makes call with a single
-                                                                     NIP every 0.1 second
-                                                                     https://www.podatki.gov.pl/media/3275/specyfikacja-we-wy.pdf */ 
-          function timer(){
-            var url = 'https://sprawdz-status-vat.mf.gov.pl/?wsdl';  //API adress
-            var args = { nip: data[0] };                             //NIP as batch to the SOAP
+  let dataObj = {
+    startValues: '',                        //Placeholder for data received through POST
+    results: [],                            //Placeholder for results
+    date: new Date().formatDate(),          //Placeholder for current date
+    show: function(){                       //Log stored data in console
+      let tmp = this.results;
+      for(let i = 0; i < tmp.length; i++){
+        let info = `Element ${i} {\n\tkod: ${tmp[i].kod},\n\tkomunikat: ${tmp[i].komunikat},\n\tdate: ${tmp[i].date}\n\tnip: ${tmp[i].nip}\n\tOrigin: ${tmp[i].origin}\n}\n`
+        console.log(info);
+      }
+    },
+    commFromCode: function(code){            //Get a messeage based on the NIP number status
+      switch (code) {
+        case 'N':
+          return 'Podmiot o podanym identyfikatorze podatkowym NIP nie jest zarejestrowany jako podatnik VAT';
+          break;
+        case 'C':
+          return 'Podmiot o podanym identyfikatorze podatkowym NIP jest zarejestrowany jako podatnik VAT czynny';
+          break;
+        case 'Z':
+          return 'Podmiot o podanym identyfikatorze podatkowym NIP jest zarejestrowany jako podatnik VAT zwolniony';
+          break;
+        case 'I':
+          return 'Błąd zapytania - Nieprawidłowy Numer Identyfikacji Podatkowej';
+          break;
+        case 'D':
+          return 'Błąd zapytania - Data spoza ustalonego zakresu';
+          break;
+        case 'X':
+          return 'Usługa nieaktywna';
+          break;
+        default:
 
+      }
+    },
+    addResult: function(obj){                //Add data to 'results' attribute in form of an object
+      this.results.push({kod: obj.status, komunikat: obj.comm, nip: obj.nip, date: obj.date, origin: obj.origin});
+    },
+    callAPI: function(data){
+      //.. Function making a proper call to the API
+      async function call(nip){
+        var url = 'https://sprawdz-status-vat.mf.gov.pl/?wsdl'; //API adress
+
+        let promise = new Promise((resolve, rej) => {
+          setTimeout(function(){
             soap.createClient(url, function(err, client) {
-              client.SprawdzNIP({ nip: data[0] }, function(err, result) {
-                dataObj.add(result, data[0]);          //Save result and nip for this result to array
-                data.shift();                          //Deletes first NIP in array which was processed in the same iteration
+              console.log('Calling API');
 
-                if(!data || dataObj.processed.length === ctrl){ //Are all NIPs processed?
-                  stopFunction();                               //Call to stop interval processing NIPs
+              client.SprawdzNIP({ nip: nip }, function(err, result) {
+                let tmpObj = {                //Prepares temporary object to be added to the 'results' attribute
+                  status: result.Kod,
+                  comm: result.Komunikat,
+                  nip: nip,
+                  date: dataObj.date,
+                  origin: 'API'
                 }
+
+                dataObj.addResult(tmpObj);    //Save result
+                resolve(tmpObj);              //Returns result. It's redundant
               });
             });
-          }
+          }, 100)                             //Making calls with at least 100ms delay. (Documentation states 10 calls per second)
+        });
 
-          //Stops NIP processing interval
-          function stopFunction() {
-            clearInterval(timedCalls); //Clear interval making calls to api
-            dataObj.finish();          //Call obj method to send data back to client
-            return;
-          }
-        }
+        let fin = await promise;              //Wait for the resolve of the promise
       }
 
-      dataObj.startCalls(data); //Start processing NIPs
+      function recurseCalls(param){           //Make one call. Wait for response. Shift data. Recurses till parameter is not empty.
+        if(param[0] != undefined){            //Checks if first parameter isn't 'undefined'. Shit happens.
+          call(param[0]).then(function(){
+            param.splice(0, 1);               //Shift data after the call
+
+            if(param.length){
+              recurseCalls(param);
+            }
+            else{
+              DBobj.insertNIP(dataObj.results); //Insert results to DB
+              console.log('all data processed');
+              dataObj.show();
+            }
+          });
+        }
+        else{                                 //Whole else will be changed. Test only.
+          param.splice(0, 1);
+          if(param.length){
+            recurseCalls(param);
+          }
+          else{
+            DBobj.insertNIP(dataObj.results);
+            console.log('all data processed');
+            dataObj.show();
+          }
+        }
+
+      }
+
+      recurseCalls(data);                     //Start calls to API
+    },
+    finalize: function(){
+      res.end(JSON.stringify(this.results));      //Return data to client as JSON
+      res.status(200).send();                     //Everything's OK
+      console.log('stopped');
     }
   }
+
+  let DBobj = {
+    connection: mysql.createConnection({                //Connection DB data
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'systemy_zintegrowane'
+    }),
+    connect: function(){                                //Open connection with DB
+      let flag = false;                                 //Flag to determine if connection succeeded
+
+      this.connection.connect(function(err) {
+        if (err){
+          flag = false;
+          console.log('Not connected to DB');
+        }
+        else{
+          flag = true;
+          console.log('Connected to DB');
+        }
+      });
+
+      return flag;                                        //Possibility to check if connection failed while using the moethod
+    },
+    kill: function(){                                     //Close connection with DB
+      this.connection.end();
+      console.log('Closed DB connection');
+    },
+    checkNIP: function(data){                             //data = nip numbers (array), date = check status of given nip for given date
+      let date = dataObj.date;                            //Get current date
+      let query = `SELECT * FROM status_nip WHERE `;      //Placeholder for query to DB
+
+      data.forEach((entry) => {                           //Add condition to search for results in DB based on given NIP number and current date
+        query += `(nip = ${entry} AND data = '${date}') OR `;
+      });
+
+      query = query.substring(0, query.length - 4) + `;`; //Trims last 'OR' and adds ';'
+
+      this.connection.query(query, (err,rows) => {        //Query to DB
+        if(err){
+          console.log('Query failed');
+          throw err;
+        }
+        else{
+          console.log('Query successful');
+
+          if(rows.length){
+            console.log(`Data length received from Db: ${rows.length}`);
+
+            rows.forEach((entry) => {
+              let tmp = {                                 //Prepares temporary object to be added to the 'results' attribute
+                status: entry.status,
+                comm: dataObj.commFromCode(entry.status),
+                nip: entry.nip,
+                date: entry.data.formatDate(),
+                origin: 'DB'
+              }
+
+              dataObj.addResult(tmp);                     //Add data to 'results' attribute
+
+              data.splice(data.indexOf(entry.nip), 1);    //Remove from NIP array numbers found in DB
+            });
+          }
+          if(data){
+            console.log(data);
+            dataObj.callAPI(data);                        //If there're some NIP numbers left get data from API
+          }
+          else{
+            dataObj.finalize();                           //No data to process left. Send response to client
+          }
+        }
+      });
+    },
+    insertNIP: function(data){
+      let flag = false;                                   //Flag to determine if there's data received from API
+      let query = `INSERT INTO status_nip VALUES `;       //Placeholder for query to DB
+
+      data.forEach((entry) => {
+        if(entry.origin === 'API'){
+          query += `('${entry.nip}','${entry.date}','${entry.kod}'),`;
+          flag = true;
+        }
+      });
+
+      if(flag){
+        query = query.substring(0, query.length - 1) + `;`;
+
+        this.connection.query(query, (err,rows) => {      //Insert data from API to DB
+          this.kill();                                    //End connection with DB
+
+          if(err){
+            console.log('Query failed');
+            throw err;
+          }
+          else{
+            console.log('Query successful');
+          }
+
+          dataObj.finalize();
+        });
+      }
+      else {
+        this.kill();                                      //End connection with DB
+        console.log('No data needs to be sent to DB');
+        dataObj.finalize();                               //Send response to client
+      }
+    }
+  }
+
+  req.on('data', chunk => {                               //Data received through POST from client in form of plain text
+      dataObj.startValues = chunk.toString().split(' ');  //Split data on spaces into an array
+
+      DBobj.checkNIP(dataObj.startValues, dataObj.date);  //Start by checking given data in DB
+  });
 });
